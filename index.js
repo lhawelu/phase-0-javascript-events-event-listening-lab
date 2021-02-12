46a0d97c64b6470abe2f7cd4f90ec7a7a7870523
Larry Hawelu <lhawelu@gmail.com>
2021-02-12 14:25:24 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
 function addingEventListener() {
+  const input = document.getElementById('input');
+  const anotherInput = document.querySelector('div div div div div')
+
+  function clickAlert() {
+    alert('I was clicked!');
+  }
+
+  input.addEventListener('click', clickAlert); 
+  anotherInput.addEventListener('click', clickAlert); 
 }
+
+addingEventListener();
